516991ce76fcd6a1412b738cf7134cb5ecea3e12
Aaron Graniel <105092151+Aaron2105@users.noreply.github.com>
2023-03-31 00:03:21 -0600

    Listo

--- MetodosOrdenamiento/RadixSort.java
+++ MetodosOrdenamiento/RadixSort.java
@@ -1,21 +1,42 @@
-
+/**
+ * Clase para ordenar Lista doblemente ligada de arreglos de Strings mediante el algoritmo de Radix Sort
+ * @author Aaron Graniel, Daniel Garcia
+ * @version 1.0
+ */
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 class RadixSort {
-  
+	
+	/**
+	 *  * lista de los datos que se quieren ordenar
+	 */
 	private LinkedList<ArrayList<String>> array = new LinkedList<ArrayList<String>>();
 
+	/**
+	 * Crea un objeto RadixSort. 
+	 * @param lista La lista de elementos que se quiere ordenar.
+	 */
 	public RadixSort(LinkedList<ArrayList<String>> lista){
-		for(int i = 0; i < lista.size();i++) {
-			array.add(i, lista.get(i));
-		}
+		this.array = lista;
 	}
 
+	/**
+	 * Metodo para obtener la list de los elementos.
+	 * @return <ul>
+     *         <li>La lista ordena, si hemos invocamos previamente el metodo radixSort</li>
+     *         <li>La lista sin ordenar, si no hemos invocamos el metodo radixSort</li>
+     *         </ul>
+	 */
 	public LinkedList<ArrayList<String>> getList(){
 		return array;
 	}
 
+	/**
+	 * Ordena una lista de cadenas utilizando el algoritmo de ordenación de counting sort en orden ascendente, según el carácter en la posición dada. 
+	 * @param place la posición del carácter en cada cadena a tener en cuenta para la ordenación
+	 * @param key el índice del arreglo que se usará para la ordenación
+	 */
 	public void countingSortStrASC(int place, int key) {
 		LinkedList<ArrayList<String>> temp = new LinkedList<ArrayList<String>>();
         for(int i = 0; i < array.size();i++){
@@ -39,6 +60,11 @@ class RadixSort {
         array.addAll(temp);
 	}
 
+	/**
+	 * Ordena una lista de cadenas utilizando el algoritmo de ordenación de counting sort en orden descendente, según el carácter en la posición dada. 
+	 * @param place la posición del carácter en cada cadena a tener en cuenta para la ordenación
+	 * @param key el índice del arreglo que se usará para la ordenación
+	 */
 	public void countingSortStrDES(int place, int key) {
 		LinkedList<ArrayList<String>> temp = new LinkedList<ArrayList<String>>();
         for(int i = 0; i < array.size();i++){
@@ -62,6 +88,12 @@ class RadixSort {
         array.addAll(temp);
 	}
 
+
+	/**
+	 * Obtiene la longitud mas larga de una String de la lista de elementos
+	 * @param key el índice del arreglo que se usará para la ordenación
+	 * @return  la longitud mas larga de una String de la lista
+	 */
 	public int getMaxLength(int key) {
 		int maxLen = 0;
         for (ArrayList<String> arrayList : array) {
@@ -74,7 +106,12 @@ class RadixSort {
         return maxLen;
 	}
 
-	public void countingSortNumASC(int size, int place, int key) {
+	/**
+	 * Ordena una lista de numeros utilizando el algoritmo de ordenación de counting sort, según una posición de un dígito a considerar.
+	 * @param place la posición del dígito a considerar en el ordenamiento
+	 * @param key el índice del arreglo que se usará para la ordenación
+	 */
+	public void countingSortNum(int place, int key) {
 		LinkedList<ArrayList<String>> output = new LinkedList<ArrayList<String>>();
 		for (int i = 0; i < 1999; i++) {
 			output.add(new ArrayList<String>());
@@ -91,7 +128,7 @@ class RadixSort {
 		for (int i = 1; i < 10; i++)
 		  count[i] += count[i - 1];
 	
-		for (int i = size - 1; i >= 0; i--) {
+		for (int i = array.size() - 1; i >= 0; i--) {
 			if(Double.parseDouble(array.get(i).get(key))<0){
 				output.set(count[-1*(int)((Double.parseDouble(array.get(i).get(key)) / place) % 10)]-1, array.get(i));
 		  		count[-1*(int)((Double.parseDouble(array.get(i).get(key)) / place) % 10)]--;
@@ -102,11 +139,17 @@ class RadixSort {
 		  	
 		}
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < array.size(); i++)
 			array.set(i, output.get(i));
 
 	}
 
+	/**
+	 * Obtiene el numero mas grande de lista de elementos
+	 * @param n el tamaño del lista
+	 * @param key el índice del arreglo que se usará para la ordenación
+	 * @return  el numero mas grande de lista de elementos
+	 */
 	public double getMax(int n, int key) {
 		double max = Double.parseDouble(array.get(0).get(key));
 		for (int i = 1; i < n; i++)
@@ -115,14 +158,19 @@ class RadixSort {
 		return max;
 	}
 
-	public void radixSort(int size, int key,int order, int tipo) {
+	/**
+	 * Ordena la lista, previamente proporcionada durante la creacion del objeto RadixSort, por el valor de una llave específica usando el algoritmo de ordenamiento Radix Sort.
+	 * @param key el indice del arrayList que se usará para la ordenación de toda la lista
+	 * @param order el orden en que se desea ordenar el ArrayList (1 para ascendente, 2 para descendente).
+	 * @param tipo el tipo de datos que se quiere ordenar (1 para String, 2 para Double).
+	 */
+	public void radixSort(int key,int order, int tipo) {
 
 		if(tipo == 2){
-			size = array.size();
-			double max = getMax(size, key);
+			double max = getMax(array.size(), key);
 
 			for (double place = 1; max / place > 0; place *= 10){
-				countingSortNumASC(size, (int)place, key);
+				countingSortNum((int)place, key);
 			}
 			if(order == 2) {
 				LinkedList<ArrayList<String>> temp = new LinkedList<ArrayList<String>>();
